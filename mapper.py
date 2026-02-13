82ff01c7a816d85e11467ef445c14c1192d97fc6
StefanoWind <ltzsfn@gmail.com>
2026-02-12 18:04:54 -0700

    fixed defition of probability

--- mapper.py
+++ mapper.py
@@ -5,26 +5,141 @@ Probabilistic mapper of resource
 import os
 cd=os.getcwd()
 import numpy as np
-from matplotlib import pyplot as plt
-import warnings
-import pandas as pd
-import scipy as sp
+import shapely
+from shapely.ops import unary_union
 import matplotlib
-import re
-import matplotlib.gridspec as gridspec
+from matplotlib import pyplot as plt
 import geopandas as gpd
-import xarray as xr
 matplotlib.rcParams['font.family'] = 'serif'
 matplotlib.rcParams['mathtext.fontset'] = 'cm' 
 matplotlib.rcParams['font.size'] = 12
 matplotlib.rcParams['savefig.dpi'] = 300
 plt.style.use("seaborn-v0_8-bright")
 plt.close('all')
-warnings.filterwarnings('ignore')
-
 
 #%% Inputs
 source=os.path.join(cd,'data','proprietary.gpkg')
 
+rock1={'rock_class':'ultramafic','rock_type':'serpentinite'}
+rock2={'rock_type':'granodioritic'}
+
+xmin=480000
+xmax=640000
+ymin=5520000
+ymax=5680000
+dx=1000
+dy=1000
+fall_off=10000
+
+#%%Functions
+def probability(distance1,distance2,fall_off,p0=1,tolerance=0.05):
+    from scipy.special import erfinv
+    '''
+    Gaussian probability shape function
+    
+    INPUTS:
+    distance: Euclidean distance from interface
+    p0: maximum probability
+    cut_out: fall-off distance
+    tolerance: fraction of probability integral outside fall-off (double-sided)
+    '''
+    sigma=fall_off/(2**0.5*erfinv(1-tolerance))/2
+    prob=p0*np.exp(-distance1**2/(2*sigma**2))*np.exp(-distance2**2/(2*sigma**2))
+    
+    return prob
+
+def distance_map(points,X,Y):
+    grid = shapely.points(X.ravel(), Y.ravel())
+    distance =  shapely.distance(points, grid).reshape(X.shape)
+    
+    return distance
+
 #%% Initialization
 Data = gpd.read_file(source)
+n=len(Data.index)
+
+#data selection
+sel1=0
+for c in rock1:
+    sel1+=np.array([rock1[c] in Data[c].iloc[i] for i in range(n)])
+sel1=np.where(sel1>0)[0]
+
+sel2=0
+for c in rock2:
+    sel2+=np.array([rock2[c] in Data[c].iloc[i] for i in range(n)])
+sel2=np.where(sel2>0)[0]
+
+x=np.arange(xmin,xmax+dx/1,dx)
+y=np.arange(ymin,ymax+dy/1,dy)
+X,Y=np.meshgrid(x,y,indexing='ij')
+
+#%% Main
+
+poly1=Data['geometry'].iloc[sel1]
+poly2=Data['geometry'].iloc[sel2]
+
+#calculate distance grid
+distance1=distance_map(unary_union(poly1),X,Y)
+distance2=distance_map(unary_union(poly2),X,Y)
+
+#probability map using distance from nearest interface
+prob_near=probability(distance1,distance2,fall_off)
+
+#probability map superposing individual pair contributions
+prob_inv=1
+ctr=0
+for p1 in poly1:
+    distance1=distance_map(p1,X,Y)
+    for p2 in poly2:
+        distance2=distance_map(p2,X,Y)
+        prob_inv*=1-probability(distance1,distance2,fall_off)
+    ctr+=1
+    print(f'{ctr}/{len(sel1)} polygons done')
+    
+prob_cum=1-prob_inv
+
+#%% Plots
+#heat map
+fig = plt.figure(figsize=(18,10))
+plt.pcolor(x/1000,y/1000,prob_near.T,cmap=r'Greys',vmin=0,vmax=1)
+for p1 in poly1:
+    x1, y1 = p1.exterior.xy
+    plt.plot(np.array(x1)/1000, np.array(y1)/1000, color='b')
+    plt.fill(np.array(x1)/1000, np.array(y1)/1000, color='b',alpha=0.2)
+
+for p2 in poly2:
+    x2, y2 = p2.exterior.xy
+    plt.plot(np.array(x2)/1000, np.array(y2)/1000, color='orange')
+    plt.fill(np.array(x2)/1000, np.array(y2)/1000, color='orange',alpha=0.2)
+    
+plt.grid()
+plt.xlim([xmin/1000,xmax/1000])
+plt.ylim([ymin/1000,ymax/1000])
+plt.xticks(np.arange(xmin/1000,xmax/1000,fall_off/1000))
+plt.yticks(np.arange(ymin/1000,ymax/1000,fall_off/1000))
+plt.gca().set_aspect("equal")
+plt.xlabel(r'$x$ [km]')
+plt.ylabel(r'$y$ [km]')
+plt.colorbar(label='Probability (nearest)')
+
+fig = plt.figure(figsize=(18,10))
+plt.pcolor(x/1000,y/1000,prob_cum.T,cmap=r'Greys',vmin=0,vmax=1)
+for p1 in poly1:
+    x1, y1 = p1.exterior.xy
+    plt.plot(np.array(x1)/1000, np.array(y1)/1000, color='b')
+    plt.fill(np.array(x1)/1000, np.array(y1)/1000, color='b',alpha=0.2)
+
+for p2 in poly2:
+    x2, y2 = p2.exterior.xy
+    plt.plot(np.array(x2)/1000, np.array(y2)/1000, color='orange')
+    plt.fill(np.array(x2)/1000, np.array(y2)/1000, color='orange',alpha=0.2)
+    
+plt.grid()
+plt.xlim([xmin/1000,xmax/1000])
+plt.ylim([ymin/1000,ymax/1000])
+plt.xticks(np.arange(xmin/1000,xmax/1000,fall_off/1000))
+plt.yticks(np.arange(ymin/1000,ymax/1000,fall_off/1000))
+plt.gca().set_aspect("equal")
+plt.xlabel(r'$x$ [km]')
+plt.ylabel(r'$y$ [km]')
+plt.colorbar(label='Probability (cumulated)')
--- test.py
+++ test.py
@@ -4,60 +4,164 @@ Test mapping operations
 """
 
 from shapely.geometry import Polygon
+from shapely.ops import unary_union
 import shapely
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.rcParams['font.family'] = 'serif'
+matplotlib.rcParams['mathtext.fontset'] = 'cm' 
+matplotlib.rcParams['font.size'] = 14
+matplotlib.rcParams['savefig.dpi'] = 300
+
 plt.close('all')
 
 #%% Inputs
-p1 = Polygon([(0,0), (1,0), (1,1), (0,1)])
-p2 = Polygon([(1,0), (2,0), (2,1), (1,1)]) 
-xmin=-2
+poly1 = [Polygon([(-1,0),(0,0.5),(0,0.5),(0,1),(-1,1),(-1,0)]),
+      Polygon([(-2.5,-1.5),(-1.25,-1.5),(-1.25,1),(-2.5,-1)])]
+poly2 = [Polygon([(0,-1),(0,1),(1,1),(1,-1)]),
+      Polygon([(-1,-2),(-1,0),(0,0),(-1,-2)])] 
+
+xmin=-3
 xmax=2
-ymin=-1.5
+ymin=-2
 ymax=1.5
-dx=0.1
-dy=0.1
+dx=0.01
+dy=0.01
 
-sigma=0.25
+fall_off=0.5
 
+#%%Functions
+def probability(distance1,distance2,fall_off,p0=1,tolerance=0.05):
+    from scipy.special import erfinv
+    '''
+    Gaussian probability shape function
+    
+    INPUTS:
+    distance: Euclidean distance from interface
+    p0: maximum probability
+    cut_out: fall-off distance
+    tolerance: fraction of probability integral outside fall-off (double-sided)
+    '''
+    sigma=fall_off/(2**0.5*erfinv(1-tolerance))/2
+    prob=p0*np.exp(-distance1**2/(2*sigma**2))*np.exp(-distance2**2/(2*sigma**2))
+    
+    return prob
+
+def distance_map(points,X,Y):
+    grid = shapely.points(X.ravel(), Y.ravel())
+    distance =  shapely.distance(points, grid).reshape(X.shape)
+    
+    return distance
+
+    
 #%% Initialization
 x=np.arange(xmin,xmax+dx/1,dx)
 y=np.arange(ymin,ymax+dy/1,dy)
 X,Y=np.meshgrid(x,y,indexing='ij')
 
 #%% Main
-p_int=p1.intersection(p2)
 
-mask = shapely.contains_xy(p_int.buffer((dx**2+dy**2)**0.5/2), X, Y)
+#calculate distance grid
+distance1=distance_map(unary_union(poly1),X,Y)
+distance2=distance_map(unary_union(poly2),X,Y)
 
-prob_inv=np.ones_like(X)
-for i,j in zip(*np.where(mask)):
-    R=((X-x[i])**2+(Y-y[j])**2)**0.5
-    prob_inv*=(1-np.exp(-R**2/(2*sigma**2)))
-    
-prob=1-prob_inv
+#probability map using distance from nearest interface
+prob_near=probability(distance1,distance2,fall_off)
+
+#probability map superposing individual pair contributions
+prob_inv=1
+for p1 in poly1:
+    distance1=distance_map(p1,X,Y)
+    for p2 in poly2:
+        distance2=distance_map(p2,X,Y)
+        prob_inv*=1-probability(distance1,distance2,fall_off)
+
+prob_cum=1-prob_inv
 
 #%% Plots
-fig, ax = plt.subplots()
-plt.pcolor(x,y,prob.T,cmap='RdYlGn',edgecolor=(0,0,0,0.1))
-# polygon 1
-x1, y1 = p1.exterior.xy
-ax.fill(x1, y1, alpha=0.5, label="Polygon 1")
 
-# polygon 2
-x2, y2 = p2.exterior.xy
-ax.fill(x2, y2, alpha=0.5, label="Polygon 2")
+#probability function
+cmap = plt.cm.jet
+dist_plot=np.arange(0,1.1,0.1)
+colors = cmap(np.linspace(0, 1, len(dist_plot)))
+plt.figure()
+x_plot=np.arange(-1,1.1,0.001)
+ctr=0
+for d in dist_plot:
+    prob_plot=probability(x_plot-d/2,x_plot+d/2, fall_off)
+    plt.plot(x_plot,prob_plot,color=colors[ctr],label=f'Distance = {d:.2f}')
+    plt.plot(-d/2,1,'.',color=colors[ctr])
+    plt.plot(d/2,1,'.',color=colors[ctr])
 
-# intersection (if any)
-if not p_int.is_empty:
-    if p_int.geom_type == "Polygon":
-        x_int, y_int = p_int.exterior.xy
-        ax.fill(x_int, y_int, color="red", alpha=0.8, label="Intersection")
-    else:
-        ax.plot(*p_int.xy, color="k", linewidth=2, label="Intersection")
+    ctr+=1
+plt.title(f'Fall off distance = {fall_off}')
+plt.xlabel('$x$')
+plt.ylabel('$P$')
+plt.grid()
+plt.legend()
 
+#heat map
+fig = plt.figure(figsize=(18,4))
+ax=plt.subplot(1,3,1)
+plt.pcolor(x,y,prob_near.T,cmap='Greys',vmin=0,vmax=1)
+for p1 in poly1:
+    x1, y1 = p1.exterior.xy
+    plt.plot(np.array(x1), np.array(y1), color='b')
+    plt.fill(np.array(x1), np.array(y1), color='b',alpha=0.2)
+
+for p2 in poly2:
+    x2, y2 = p2.exterior.xy
+    plt.plot(np.array(x2), np.array(y2), color='orange')
+    plt.fill(np.array(x2), np.array(y2), color='orange',alpha=0.2)
+    
+plt.grid()
+plt.xlim([xmin,xmax])
+plt.ylim([ymin,ymax])
 ax.set_aspect("equal")
-ax.legend()
-plt.show
+plt.xlabel(r'$x$')
+plt.ylabel(r'$y$')
+plt.colorbar(label='Probability (nearest)')
 
+ax=plt.subplot(1,3,2)
+plt.pcolor(x,y,prob_cum.T,cmap='Greys',vmin=0,vmax=1)
+for p1 in poly1:
+    x1, y1 = p1.exterior.xy
+    plt.plot(np.array(x1), np.array(y1), color='b')
+    plt.fill(np.array(x1), np.array(y1), color='b',alpha=0.2)
+
+for p2 in poly2:
+    x2, y2 = p2.exterior.xy
+    plt.plot(np.array(x2), np.array(y2), color='orange')
+    plt.fill(np.array(x2), np.array(y2), color='orange',alpha=0.2)
+
+plt.grid()
+plt.xlim([xmin,xmax])
+plt.ylim([ymin,ymax])
+plt.xticks(np.arange(xmin,xmax,fall_off))
+plt.yticks(np.arange(ymin,ymax,fall_off))
+ax.set_aspect("equal")
+plt.xlabel(r'$x$')
+plt.colorbar(label='Probability (cumulated)')
+
+ax=plt.subplot(1,3,3)
+plt.pcolor(x,y,prob_cum.T-prob_near.T,cmap='seismic',vmin=-0.2,vmax=0.2)
+for p1 in poly1:
+    x1, y1 = p1.exterior.xy
+    plt.plot(np.array(x1), np.array(y1), color='b')
+    plt.fill(np.array(x1), np.array(y1), color='b',alpha=0.2)
+
+for p2 in poly2:
+    x2, y2 = p2.exterior.xy
+    plt.plot(np.array(x2), np.array(y2), color='orange')
+    plt.fill(np.array(x2), np.array(y2), color='orange',alpha=0.2)
+
+plt.grid()
+plt.xlim([xmin,xmax])
+plt.ylim([ymin,ymax])
+plt.xticks(np.arange(xmin,xmax,fall_off))
+plt.yticks(np.arange(ymin,ymax,fall_off))
+ax.set_aspect("equal")
+plt.xlabel(r'$x$')
+plt.colorbar(label='Probability difference \n (cumulated-nearest)')
+plt.tight_layout()
