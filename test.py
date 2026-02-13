fe371cdacea9bff8decfa9d1488d475f5e674dd3
StefanoWind <ltzsfn@gmail.com>
2026-02-13 10:47:56 -0700

    finalized test

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 Test mapping operations
+
+INPUTS:
+    poly1 (list of shapely polygons): list of polygons of type 1 [arbitrary units]
+    poly2 (list of shapely polygons): list of polygons of type 2 [arbitrary units]
+    xmin (float): minimum x [arbitrary units]
+    xmax (float): maximum x [arbitrary units]
+    ymin (float): minimum y [arbitrary units]
+    ymax (float): maximum y [arbitrary units]
+    dx (float>0): grid resolution in x [arbitrary units]
+    dy (float>0): grid resolution in y [arbitrary units]
+    fall_off (float>0): fall-off distance of probability [arbitrary units]
 """
 
 from shapely.geometry import Polygon
@@ -17,11 +28,14 @@ matplotlib.rcParams['savefig.dpi'] = 300
 plt.close('all')
 
 #%% Inputs
+
+#define lists of polygons
 poly1 = [Polygon([(-1,0),(0,0.5),(0,0.5),(0,1),(-1,1),(-1,0)]),
       Polygon([(-2.5,-1.5),(-1.25,-1.5),(-1.25,1),(-2.5,-1)])]
 poly2 = [Polygon([(0,-1),(0,1),(1,1),(1,-1)]),
       Polygon([(-1,-2),(-1,0),(0,0),(-1,-2)])] 
 
+#grid
 xmin=-3
 xmax=2
 ymin=-2
@@ -29,26 +43,39 @@ ymax=1.5
 dx=0.01
 dy=0.01
 
-fall_off=0.5
+fall_off=0.5#fall-off distance
 
 #%%Functions
 def probability(distance1,distance2,fall_off,p0=1,tolerance=0.05):
     from scipy.special import erfinv
     '''
-    Gaussian probability shape function
+    Calculate probability using double Gaussian shape function
     
     INPUTS:
-    distance: Euclidean distance from interface
-    p0: maximum probability
-    cut_out: fall-off distance
-    tolerance: fraction of probability integral outside fall-off (double-sided)
+        distance1 (np.array): map of Euclidean distance from nearest type 1 polygon [arbitrary units]
+        distance2 (np.array): map of Euclidean distance from nearest type 2 polygon [arbitrary units]
+        fall_off (float): fall-off distance [arbitrary units]
+        p0 (0<float<1): maximum probability
+        tolerance (0<float<1): fraction of integral of each Gaussian leaking outside of half the fall-off distance (double-sided)
+    
+    OUTPUTS:
+        prob: probability map 
     '''
-    sigma=fall_off/(2**0.5*erfinv(1-tolerance))/2
-    prob=p0*np.exp(-distance1**2/(2*sigma**2))*np.exp(-distance2**2/(2*sigma**2))
+    
+    sigma=fall_off/(2**0.5*erfinv(1-tolerance))/2#calculate Gaussian spread for given tolerance/fall-off
+    prob=p0*np.exp(-distance1**2/(2*sigma**2))*np.exp(-distance2**2/(2*sigma**2))#probability function
     
     return prob
 
 def distance_map(points,X,Y):
+    '''
+    Build of distance
+    INPUTS:
+        points (shapely points): points from which minimum distance is calculated
+        X,Y (np.array): grid [arbitrary units]
+    OUTPUTS:
+        distance (np.array): map of minimum distance [arbitrary units]
+    '''
     grid = shapely.points(X.ravel(), Y.ravel())
     distance =  shapely.distance(points, grid).reshape(X.shape)
     
@@ -101,8 +128,8 @@ plt.ylabel('$P$')
 plt.grid()
 plt.legend()
 
-#heat map
-fig = plt.figure(figsize=(18,4))
+#heat maps
+fig = plt.figure(figsize=(18,3))
 ax=plt.subplot(1,3,1)
 plt.pcolor(x,y,prob_near.T,cmap='Greys',vmin=0,vmax=1)
 for p1 in poly1:
@@ -118,6 +145,8 @@ for p2 in poly2:
 plt.grid()
 plt.xlim([xmin,xmax])
 plt.ylim([ymin,ymax])
+plt.xticks(np.arange(xmin,xmax,fall_off),rotation=90)
+plt.yticks(np.arange(ymin,ymax,fall_off))
 ax.set_aspect("equal")
 plt.xlabel(r'$x$')
 plt.ylabel(r'$y$')
@@ -138,7 +167,7 @@ for p2 in poly2:
 plt.grid()
 plt.xlim([xmin,xmax])
 plt.ylim([ymin,ymax])
-plt.xticks(np.arange(xmin,xmax,fall_off))
+plt.xticks(np.arange(xmin,xmax,fall_off),rotation=90)
 plt.yticks(np.arange(ymin,ymax,fall_off))
 ax.set_aspect("equal")
 plt.xlabel(r'$x$')
@@ -159,7 +188,7 @@ for p2 in poly2:
 plt.grid()
 plt.xlim([xmin,xmax])
 plt.ylim([ymin,ymax])
-plt.xticks(np.arange(xmin,xmax,fall_off))
+plt.xticks(np.arange(xmin,xmax,fall_off),rotation=90)
 plt.yticks(np.arange(ymin,ymax,fall_off))
 ax.set_aspect("equal")
 plt.xlabel(r'$x$')
